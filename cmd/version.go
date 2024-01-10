e81a36a4bb8b6453c2e97dcba6781aff6f9089c5
Neil Roza <b0h1rq47@duck.com>
2024-01-10 16:14:09 +0000

    edit cmd/version.go: smaller and stupider

--- cmd/version.go
+++ cmd/version.go
@@ -11,35 +11,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "print version information",
-	Long:  `Print version information.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		info, ok := debug.ReadBuildInfo()
-		if !ok {
-			return errors.New("debug.ReadBuildInfo()")
-		}
-		if verbose {
-			fmt.Println(info)
-		} else {
-			fmt.Printf("%v %v %v\n", info.Main.Path, info.Main.Version, info.Main.Sum)
-		}
-		return nil
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "print version information",
+		Long:  `Print version information.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			info, ok := debug.ReadBuildInfo()
+			if !ok {
+				return errors.New("debug.ReadBuildInfo()")
+			}
+			if verbose {
+				fmt.Println(info)
+			} else {
+				fmt.Printf("%v %v %v\n", info.Main.Path, info.Main.Version, info.Main.Sum)
+			}
+			return nil
+		},
+	})
 }
